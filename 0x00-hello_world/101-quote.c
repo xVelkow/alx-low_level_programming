8af67b5daa89cb2ec8acb76ff8ef9dfa0721b7b4
xVelkow <redamaktoum@gmail.com>
2023-03-16 05:41:48 -0700

    last?

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'standart error'
+ *
+ * @void : The main function takes no parameters
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - ");
+	fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+	return (1);
+}
