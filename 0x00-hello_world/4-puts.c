9dc75a95bdbe2587f9e51d30ad7b0dabd6183745
xVelkow <redamaktoum@gmail.com>
2023-03-16 04:24:27 -0700

    fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/*
+ * The entry point of the program
+ *
+ * Task : 4.Hello,puts
+ *
+ * return 0 : success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
