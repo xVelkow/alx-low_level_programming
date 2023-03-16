58e64adc6ce070da4b9cdb7116b38cbacbf8c8f1
xVelkow <redamaktoum@gmail.com>
2023-03-16 04:45:30 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : '5.Hello, printf'
+ *
+ * @void : The main function takes no parameters
+ *
+ * printf : prints 'with proper grammar, but the outcome is a piece of art,'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
