581526eec5a9430490ed3b4babbcd3974d116f7a
xVelkow <redamaktoum@gmail.com>
2023-03-17 03:53:49 -0700

    combo3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Task'
+ * @void: The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = x + 1; y <= 57; y++)
+		{
+			for (z = y + 1; z <= 57; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (y == 57 && x == y - 1 && y == z - 1)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
