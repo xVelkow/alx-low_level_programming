10ca35b8ba35604d469c6eae099eacbbcbd19aad
xVelkow <redamaktoum@gmail.com>
2023-03-17 02:36:54 -0700

    ascii numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Task'
+ * @void: The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
