dcbda631ff617e8bbcd448f3711bbaaaf409859c
xVelkow <redamaktoum@gmail.com>
2023-03-17 02:30:08 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Task'
+ * @void: The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
