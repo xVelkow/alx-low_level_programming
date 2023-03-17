3cc5bfab9d92ab7fabdb84923cb350d2b8b5b809
xVelkow <redamaktoum@gmail.com>
2023-03-17 02:20:10 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Task.2'
+ * @void: The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
