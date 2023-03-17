16869a9b341983414b3aff1e73ece2491084ed62
xVelkow <redamaktoum@gmail.com>
2023-03-17 02:26:26 -0700

    can't find e or q? my bad

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Task.2'
+ * @void: The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
