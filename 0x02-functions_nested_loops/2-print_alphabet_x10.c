ab301995ef77b59d4a5f1aab0c878fd9fd6a08ee
xVelkow <redamaktoum@gmail.com>
2023-03-21 12:58:12 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints a to z 10 times
+ *
+ * Description: 'task 2'
+ *
+ * @void: does not take any parameters
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	for (; i < 10; i++)
+	{
+		for(; c <= 'z'; c++)
+		{
+			_putchar(c);
+	
+		}
+		c = 'a';
+		_putchar('\n');
+	}
+}
