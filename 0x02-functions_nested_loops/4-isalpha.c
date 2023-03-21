0a5ba6180e9781a170d9c2b5132148072d2e3689
xVelkow <redamaktoum@gmail.com>
2023-03-21 13:27:09 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _isalpha - prints 1 or 0
+ *
+ * Description: 'task 4'
+ *
+ * @c: takes a number
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	char a = 'A';
+        char n = 'Z';
+
+	for (; a <= n; a++)
+	{
+		if (a == c)
+			return 1;
+		if (a == 'Z')
+		{
+			a += 7;
+			n = 'z';
+		}
+	}
+	return 0;
+}
