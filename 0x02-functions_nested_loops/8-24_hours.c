a50a69b9d2988cd621d08da8f0f2b32495ae35a0
xVelkow <redamaktoum@gmail.com>
2023-03-21 14:38:39 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,27 +11,22 @@
  */
 void jack_bauer(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int a = 0;
+	int b = 0;
 
-	for (a = 0; a < 3; a++)
+	while (a <= 23)
 	{
-		for (b = 0; b < 4; b++)
+		while(b <= 59)
 		{
-			for (c = 0; c < 6; c++)
-			{
-				for (d = 0; d < 10; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
 		}
+		b = 0;
+		a++;
 	}
 }
