f20b88d836c1ca63759bf8be8c90135a5dbbf53d
xVelkow <redamaktoum@gmail.com>
2023-03-21 15:26:08 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - takes to arguments and add them
+ *
+ * Description: 'task 10'
+ *
+ * @a: does take an integer
+ * @b: does take an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a,int b)
+{
+	return (a + b);
+}
