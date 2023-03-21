2123cfcfd1f0f421b038f17b17cccfed205d1d6e
xVelkow <redamaktoum@gmail.com>
2023-03-21 15:37:12 -0700

    fixing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,12 +16,12 @@ void print_to_98(int n)
 		printf("%d", n);
 	while (n < 98)
 	{
-		printf("%d\, ", n);
+		printf("%d, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%d\, ", n);
+		printf("%d, ", n);
 		n--;
 	}
 }
