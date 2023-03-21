383112694fb2238e33db5503cb7292dc76aedbcd
xVelkow <redamaktoum@gmail.com>
2023-03-21 13:10:50 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - prints 1 or 0 depending on c
+ *
+ * Description: 'task 3'
+ *
+ * @c: c is a character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	char a = 'a';
+	for (; a <= 'z'; a++)
+	{
+		if (a == c)
+			return (1);
+	}
+	return (0);
+}
