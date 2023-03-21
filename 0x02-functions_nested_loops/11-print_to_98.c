baf2f61e089db915a65580bb201edc6bae116b48
xVelkow <redamaktoum@gmail.com>
2023-03-21 15:40:00 -0700

    fixing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,19 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-		printf("%d", n);
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	while (n >= 98)
+	while (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
 	}
 	_putchar('\n');
 }
