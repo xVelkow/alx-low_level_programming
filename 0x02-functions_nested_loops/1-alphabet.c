1972c056b710e21646e3f0f1434b3fedb640e7b2
xVelkow <redamaktoum@gmail.com>
2023-03-21 12:42:33 -0700

    fixing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * 
  * Description: 'task 1'
+ *
  * @void: does not take any parameters
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char c = 'a';
 
-	for (; c <= 'z';c++)
+	for (; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
