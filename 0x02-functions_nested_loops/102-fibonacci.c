2042178ab5375c03823ac5989f1e4243a8851a53
xVelkow <redamaktoum@gmail.com>
2023-03-21 16:58:20 -0700

    fixing

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,18 @@
 int main(void)
 {
 	int i;
-	int y = 0;
-	int x = 1;
-	int temp;
+	long int y = 0;
+	long int x = 1;
+	long int temp;
 
 	for (i = 0; i < 50; i++)
 	{
 		temp = x;
 		x = x + y;
 		y = temp;
-		printf("%d, ", x);
+		printf("%ld", x);
+		if (i < 49)
+			putchar(',');
 	}
 	printf("\n");
 	return (0);
