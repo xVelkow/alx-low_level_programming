6e6a456ba08e1274a3fd09f2c22d2a0d57376383
xVelkow <redamaktoum@gmail.com>
2023-03-21 13:57:53 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints abs
+ *
+ * Description: 'task 6'
+ *
+ * @n: does take an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
