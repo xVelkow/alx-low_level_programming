651a173b89aeac4b7d6dd056185ee1dbe79ab1e7
xVelkow <redamaktoum@gmail.com>
2023-03-21 17:20:34 -0700

    lastFib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: the first 98 of fibonacci starting from 1 2
+ * @void: no params
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long y = 0;
+	unsigned long x = 1;
+	unsigned long temp;
+
+	for (i = 0; i < 98; i++)
+	{
+		temp = x;
+		x = x + y;
+		y = temp;
+		printf("%lu", x);
+		if (i < 49)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	printf("\n");
+	return (0);
+}
