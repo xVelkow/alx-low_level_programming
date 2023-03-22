8179bcb43caec0d4cca5ad37c33f4d9a315054d6
xVelkow <redamaktoum@gmail.com>
2023-03-22 13:03:50 -0700

    fixing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,39 @@
 #include "main.h"
-/*
-* main - Entry point
-* @n : integer
-* Return : always 0 (Success)
-*/
+
+/**
+ * times_table - prints n times table 
+ *
+ * Description: 'task 12'
+ *
+ * @n: does take an integer
+ *
+ * Return: Always 0 (Success)
+ */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int p;
+	int x = 0;
+	int y = 0;
 
-    if (n < 0 || n > 15)
-        return;
-
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            p = i * j;
-
-            if (j == 0)
-            {
-                _putchar(p + '0');
-            }
-            else
-            {
-                if (p < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else if (p < 100)
-                {
-                    _putchar(' ');
-                }
-
-                _putchar(p / 100 + '0');
-                _putchar((p / 10) % 10 + '0');
-                _putchar(p % 10 + '0');
-            }
-
-            if (j < n)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-
-	_putchar('\n');
-    }
+	for (; x < n; x++)
+	{
+		for (; y < n; y++)
+		{
+			if (((x * y) >= 10) || (x * (y + 1)) >= 10)
+			{
+				if ((x * y) >= 10)
+					_putchar(((x * y) / 10) + '0');
+				_putchar(((x * y) % 10) + '0');
+				if (y != 9)
+					_putchar(','),_putchar(' ');
+			}
+			else
+			{
+				_putchar((x * y) + '0');
+				if (y != 9)
+					_putchar(','),_putchar(' '),_putchar(' ');
+			}
+		}
+		y = 0;
+		_putchar('\n');
+	}
 }
