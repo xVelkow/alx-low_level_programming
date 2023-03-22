9ca795aeeb6fa329f4dc9c121f16db2ab29597d5
xVelkow <redamaktoum@gmail.com>
2023-03-21 19:00:22 -0700

    fixing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,52 @@
 #include "main.h"
-/**
- * print_times_table - prints times table from 0 to n
- * Description: 'task 12'
- * @n: does take an integer
- * Return: Always 0 (Success)
- */
+/*
+* main - Entry point
+* @n : integer
+* Return : always 0 (Success)
+*/
 void print_times_table(int n)
 {
-	int x = 0;
-	int y = 0;
+	int i;
+	int j;
+	int p;
 
-	if (!(n > 15 || n < 0))
-	{
-		for (; x <= n; x++)
-		{
-			for (; y <= n; y++)
-			{
-				if (((x * y) >= 10) || (x * (y + 1)) >= 10)
-				{
-					if ((x * y) >= 10 && (x * y) < 100)
-					{
-						_putchar(((x * y) / 10) + '0');
-						_putchar(((x * y) % 10) + '0');
-					}
-					else if ((x * y) >= 100)
-					{
-						_putchar(((x * y) / 100) + '0');
-						_putchar((((x * y) % 100) / 10) + '0');
-						_putchar((((x * y) % 100) % 10) + '0');
-					}
-					else
-					{
-						_putchar((x * y) + '0');
-					}
-					if (y != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						if ((x * y) < 100 && (x * (y + 1) < 100))
-							_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar((x * y) + '0');
-					if (y != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-			}
-			y = 0;
-			_putchar('\n');
-		}
-	}
+    if (n < 0 || n > 15)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            p = i * j;
+
+            if (j == 0)
+            {
+                _putchar(p + '0');
+            }
+            else
+            {
+                if (p < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else if (p < 100)
+                {
+                    _putchar(' ');
+                }
+
+                _putchar(p / 100 + '0');
+                _putchar((p / 10) % 10 + '0');
+                _putchar(p % 10 + '0');
+            }
+
+            if (j < n)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+
+	_putchar('\n');
+    }
 }
