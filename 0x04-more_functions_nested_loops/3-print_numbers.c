1efd5ccf5aac43bab4081b0500d57f1d0f8b474d
xVelkow <redamaktoum@gmail.com>
2023-03-23 08:47:06 -0700

    fixing

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - from 0 to 9
+ * Description: 'task 3'.
+ *
+ * @void: none
+ * Return: Always 0 (Success)
+ */
+void mul(void)
+{
+	int n;
+
+	for(n = 48; n <= 57; n++)
+		_putchar(n);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int largest_number(int a, int b, int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
