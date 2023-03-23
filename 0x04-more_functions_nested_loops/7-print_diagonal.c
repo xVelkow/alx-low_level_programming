a84cbee185cb25c44346245f238c82ab58890fd0
xVelkow <redamaktoum@gmail.com>
2023-03-23 14:43:02 -0700

    fixing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - prints backslashes
+ * Description: task7
+ * @n: int
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
