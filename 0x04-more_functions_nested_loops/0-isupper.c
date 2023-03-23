d98edf4366968bd1ef070dddbf7e563f64d2a40f
xVelkow <redamaktoum@gmail.com>
2023-03-23 07:59:58 -0700

    fixing

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * _isupper: check if a character is uppercase
  * Description: 'task 0'
@@ -7,7 +6,7 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 91)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
