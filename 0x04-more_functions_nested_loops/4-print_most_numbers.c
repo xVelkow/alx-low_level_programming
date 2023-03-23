56f7dedf075fd58d71e1dedc8a683fa8846d7749
xVelkow <redamaktoum@gmail.com>
2023-03-23 13:45:22 -0700

    fixing

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9 except 2 and 4
+ * Description: 'task 4'.
+ *
+ * @void: none
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n == 50 || n == 52)
+			continue;	
+		_putchar(n);
+	}
+	_putchar('\n');
+}
