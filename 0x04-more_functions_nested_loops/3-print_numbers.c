73d62b53876a25da8e82dfe487e96cbf57b7209a
xVelkow <redamaktoum@gmail.com>
2023-03-23 08:48:36 -0700

    fixing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  * @void: none
  * Return: Always 0 (Success)
  */
-void mul(void)
+void print_numbers(void)
 {
 	int n;
 
-	for(n = 48; n <= 57; n++)
+	for (n = 48; n <= 57; n++)
 		_putchar(n);
 	_putchar('\n');
 }
