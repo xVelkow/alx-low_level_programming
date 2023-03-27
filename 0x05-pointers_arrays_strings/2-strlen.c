7e031992ce6f078ece690d3474ac8816c012d37f
xVelkow <redamaktoum@gmail.com>
2023-03-27 14:59:50 -0700

    fixing

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen - length of string
+ * Description: task2
+ * @s: pointer to char
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] != '\0'; i++){}
+	return (i);
+}
