84deb767dd05a5234da5b2646820857a079d6e79
xVelkow <redamaktoum@gmail.com>
2023-03-27 15:18:09 -0700

    fixing

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts : prints
+ * Description: task3
+ * @str: pointer to char
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
