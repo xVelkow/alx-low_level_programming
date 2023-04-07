d5f696776372995fbc18f153668d4fe6b7f43c0f
xVelkow <redamaktoum@gmail.com>
2023-04-07 00:26:26 +0000

    t0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string
- * @s: pointer to char
- *
- * Return: void.
- */
+* _puts_recursion - prints a string
+* @s: pointer to char
+* Return: void.
+*/
 void _puts_recursion(char *s)
 {
     if (*s != '\0')
