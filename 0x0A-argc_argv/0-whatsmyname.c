1d12786386295e1e9bedcb604710762e04100952
xVelkow <redamaktoum@gmail.com>
2023-04-09 12:52:21 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
